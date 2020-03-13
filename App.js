b8832d3ed82ab75dd762e18cadbdb55dc1be105a
gabozubieta <60941691+gabozubieta@users.noreply.github.com>
2020-03-12 21:30:18 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Hola Mundo. Algo aprendi hoy. me voy a jugar al fulbo!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
